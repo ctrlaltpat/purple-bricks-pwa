4d39bce5fa335ee8fb47d7e1674d93b5fdc8d0f1
ssong-eu <sangyoon.song@gmail.com>
2018-12-05 10:33:54 +0000

    maps?

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Api from '../Api'
-import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Segment, Icon } from 'semantic-ui-react'
 
 export default class LoginForm extends React.Component {
 
     state = {
-        email: "",
-        password: "",
-        firstname: "",
-        lastname: ""
+        email: null,
+        password: null,
+        passwordConfirmation: null,
+        firstname: null,
+        lastname: null,
+        confirmed: false
     }
 
     handleLogin = () => {
@@ -17,12 +19,19 @@ export default class LoginForm extends React.Component {
         Api.signup(userdata).then(resp => localStorage.setItem("token", resp.token)).then(() => this.props.handleSignin())
     }
 
+    validateState = () => {
+        const { email, password, firstname, lastname, passwordConfirmation, confirmed } = this.state
+    }
+
     handleChange = (e, {name, value}) => {
         this.setState({ [name]: value })
+        if (this.state.password === this.state.passwordConfirmation && this.validateState()) {
+            this.setState({ confirmed: true })
+        }
     }
 
     render() {
-        const { email, password, firstname, lastname } = this.state
+        const { email, password, firstname, lastname, passwordConfirmation } = this.state
         return(
             <div className='login-form' style={{backgroundColor: "#6F58C9"}}>
             <style>{`
@@ -40,7 +49,8 @@ export default class LoginForm extends React.Component {
                 <Form size='large' onSubmit={this.handleLogin}>
                 <Segment stacked>
                     <Form.Input fluid icon='user' name="email" value={email} iconPosition='left' placeholder='E-mail address' onChange={this.handleChange} />
-                    <Form.Input fluid icon='lock' name="password" value={password} iconPosition='left' placeholder='Password' type='password' onChange={this.handleChange} />
+                    <Form.Input fluid icon={(this.state.password && this.state.password.length > 6) ? <Icon color="green" name="check" /> : 'lock'} name="password" value={password} iconPosition='left' placeholder='Password' type='password' onChange={this.handleChange} />
+                    <Form.Input fluid icon={this.state.password === this.state.passwordConfirmation ? <Icon color="green" name="check" /> : <Icon color="red" name="close" />} name="passwordConfirmation" value={passwordConfirmation} iconPosition='left' placeholder='Confirm Password' type='password' onChange={this.handleChange} />
                     <Form.Input fluid icon='star' name="firstname" value={firstname} iconPosition='left' placeholder='First Name' onChange={this.handleChange} />
                     <Form.Input fluid icon='star' name="lastname" value={lastname} iconPosition='left' placeholder='Last Name' onChange={this.handleChange} />
                     <Button style={{backgroundColor: "#6F58C9", color:"#fff"}} fluid size='large'>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import Auth from './Auth'
 import Signup from './Signup'
 import Dashboard from './Dashboard'
+import MapView from './MapView'
 
 
-export { Auth, Signup, Dashboard }
+export { Auth, Signup, Dashboard, MapView }
 
 
